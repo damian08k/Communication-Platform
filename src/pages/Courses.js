66c4af59b76329c30ebd9dd12c2691e49c2aaf4c
damian08k <damiankierzkowski08@wp.pl>
2020-10-19 00:35:03 +0200

    Change href attribute in <a> tag

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -4,12 +4,12 @@ import { ContentContainer } from '../components/styled/Containers';
 import { InfoText, SectionTitle } from '../components/styled/Sections';
 
 const courses = [
-  { id: '1', courseName: 'Kurs1', courseInfo: 'Informacja dot. kursu1', courseLink: '!#' },
-  { id: '2', courseName: 'Kurs2', courseInfo: 'Informacja dot. kursu2', courseLink: '!#' },
-  { id: '3', courseName: 'Kurs3', courseInfo: 'Informacja dot. kursu3', courseLink: '!#' },
-  { id: '4', courseName: 'Kurs4', courseInfo: 'Informacja dot. kursu4', courseLink: '!#' },
-  { id: '5', courseName: 'Kurs5', courseInfo: 'Informacja dot. kursu5', courseLink: '!#' },
-  { id: '6', courseName: 'Kurs6', courseInfo: 'Informacja dot. kursu6', courseLink: '!#' },
+  { id: '1', courseName: 'Kurs1', courseInfo: 'Informacja dot. kursu1', courseLink: '#!' },
+  { id: '2', courseName: 'Kurs2', courseInfo: 'Informacja dot. kursu2', courseLink: '#!' },
+  { id: '3', courseName: 'Kurs3', courseInfo: 'Informacja dot. kursu3', courseLink: '#!' },
+  { id: '4', courseName: 'Kurs4', courseInfo: 'Informacja dot. kursu4', courseLink: '#!' },
+  { id: '5', courseName: 'Kurs5', courseInfo: 'Informacja dot. kursu5', courseLink: '#!' },
+  { id: '6', courseName: 'Kurs6', courseInfo: 'Informacja dot. kursu6', courseLink: '#!' },
 ];
 
 const Courses = () => {
@@ -18,7 +18,7 @@ const Courses = () => {
       <div className="courses-info-container">
         <h3 className="courses-info-heading">{course.courseName}</h3>
         <InfoText className="courses-info-text">{course.courseInfo}</InfoText>
-        <a href={course.courseLink} rel="noopener noreferrer" target="_blank" className="courses-info-webpage">
+        <a href={course.courseLink} rel="noopener noreferrer" className="courses-info-webpage">
           Strona internetowa kursu
         </a>
       </div>
