763a4c88dc5756a7abf6a35e5c4f9ee46d1c5901
damian08k <damiankierzkowski08@wp.pl>
2020-10-20 00:42:03 +0200

    Create simple signin login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { AppContext } from '../AppProvider';
 import { ActionButton } from '../components/styled/Buttons';
 import { ContentContainer } from '../components/styled/Containers';
 import { InfoText, LoginSection, SectionTitle } from '../components/styled/Sections';
 
+import { users } from '../js/constants';
+
 import logo from '../assets/images/WZ-logo.png';
 
 const Login = () => {
+  const [inputLogin, setInputLogin] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
+  const { setIsLogged, setUserName, setUserType } = useContext(AppContext);
+
   const handleFormSubmit = e => {
     e.preventDefault();
+    checkIsLogged();
+  };
+
+  const handleLoginChange = e => {
+    setInputLogin(e.target.value);
+  };
+
+  const handlePasswordChange = e => {
+    setInputPassword(e.target.value);
+  };
+
+  const checkIsLogged = () => {
+    const isLogin = users.filter(({ login, password }) => inputLogin === login && inputPassword === password);
+    if (isLogin.length > 0) {
+      setIsLogged(true);
+      setUserName(isLogin[0].login);
+      setUserType(isLogin[0].type);
+    } else {
+      setIsLogged(false);
+    }
   };
 
   return (
@@ -22,13 +49,13 @@ const Login = () => {
           </div>
           <form className="login-form" onSubmit={handleFormSubmit}>
             <div className="login-container">
-              <input type="text" placeholder="Wprowadź numer indeksu lub login..." id="login" className="login-input" />
+              <input type="text" placeholder="Wprowadź numer indeksu lub login..." id="login" className="login-input" onChange={handleLoginChange} />
               <label htmlFor="login" className="login-label">
                 Login:
               </label>
             </div>
             <div className="login-container">
-              <input type="password" placeholder="Wprowadź hasło..." id="password" className="login-input" />
+              <input type="password" placeholder="Wprowadź hasło..." id="password" className="login-input" onChange={handlePasswordChange} />
               <label htmlFor="password" className="login-label">
                 Hasło:
               </label>
