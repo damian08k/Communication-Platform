718e529bb181af4bac37722c9f608eb605e17128
damian08k <damiankierzkowski08@wp.pl>
2020-11-06 20:01:47 +0100

    Updates

---
+++ precache-manifest.0a7e6b7feeeab511f4cb59d161096b8d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5064e03b5c7ba8fa019068395b1fc3b6",
+    "url": "/Communication-platform/index.html"
+  },
+  {
+    "revision": "29c836be6e712defc14b",
+    "url": "/Communication-platform/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "c4fca1028fbd3a5ea974",
+    "url": "/Communication-platform/static/css/main.98fc2fda.chunk.css"
+  },
+  {
+    "revision": "29c836be6e712defc14b",
+    "url": "/Communication-platform/static/js/2.3fa33b96.chunk.js"
+  },
+  {
+    "revision": "892c5a5128f54a613716d73f6973b236",
+    "url": "/Communication-platform/static/js/2.3fa33b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fca1028fbd3a5ea974",
+    "url": "/Communication-platform/static/js/main.b2a855e3.chunk.js"
+  },
+  {
+    "revision": "e881ad02e0ee20267c14",
+    "url": "/Communication-platform/static/js/runtime-main.95f3d2c5.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/Communication-platform/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "47d0ae96e7d9c0a897c6e0bf7736f2dc",
+    "url": "/Communication-platform/static/media/WZ-logo.47d0ae96.png"
+  },
+  {
+    "revision": "09455140d72448f1635ab887e3bf7240",
+    "url": "/Communication-platform/static/media/add-ico.09455140.svg"
+  },
+  {
+    "revision": "27dc3e1ed16211d1fcb6d0d72bf8906d",
+    "url": "/Communication-platform/static/media/apd-ico.27dc3e1e.svg"
+  },
+  {
+    "revision": "2c45b70be76094e9fdbd6f70b76db501",
+    "url": "/Communication-platform/static/media/attachement-ico.2c45b70b.svg"
+  },
+  {
+    "revision": "bd4b521552ed6b582e702d5b7c40b07c",
+    "url": "/Communication-platform/static/media/avatar-ico.bd4b5215.svg"
+  },
+  {
+    "revision": "4e6d3468c4a991e1b7fd2ae5ccde023f",
+    "url": "/Communication-platform/static/media/bell-ico.4e6d3468.svg"
+  },
+  {
+    "revision": "0c9e25d4c63f81ef7315dfebf29e6d95",
+    "url": "/Communication-platform/static/media/chatbot-ico.0c9e25d4.svg"
+  },
+  {
+    "revision": "2262d8ebedf3bd86a5388c11ac04b63e",
+    "url": "/Communication-platform/static/media/copyright-ico.2262d8eb.svg"
+  },
+  {
+    "revision": "c8a4b335c07f29580f18f4c167b3772d",
+    "url": "/Communication-platform/static/media/dok08_praktyki.c8a4b335.pdf"
+  },
+  {
+    "revision": "ad6be2d98ff27b140dbfa2f34accd87d",
+    "url": "/Communication-platform/static/media/edit-article-ico.ad6be2d9.svg"
+  },
+  {
+    "revision": "94cfb03f1cf96112c3a7dd42341bf4a9",
+    "url": "/Communication-platform/static/media/facebook-ico.94cfb03f.svg"
+  },
+  {
+    "revision": "b76b2ab1b6696571d148ec91f2d1dc1f",
+    "url": "/Communication-platform/static/media/for18-Cele i program praktyki.b76b2ab1.docx"
+  },
+  {
+    "revision": "6168b86fc11c0d9e25c720f9afadf4ae",
+    "url": "/Communication-platform/static/media/logout-ico.6168b86f.svg"
+  },
+  {
+    "revision": "5b09adca12c14e5973e2d4ea07100ff8",
+    "url": "/Communication-platform/static/media/messages-ico.5b09adca.svg"
+  },
+  {
+    "revision": "61b87532178bb4b61fbc78fa06debddf",
+    "url": "/Communication-platform/static/media/news-image.61b87532.jpg"
+  },
+  {
+    "revision": "eb81c70a890dea95c93ee78824155bf3",
+    "url": "/Communication-platform/static/media/remove-ico.eb81c70a.svg"
+  },
+  {
+    "revision": "69926100a639e6a3ee5d42f7227dee7a",
+    "url": "/Communication-platform/static/media/settings-ico.69926100.svg"
+  },
+  {
+    "revision": "bcc87febc43e435cbccfc4bfc4522d0a",
+    "url": "/Communication-platform/static/media/thread-ico.bcc87feb.svg"
+  },
+  {
+    "revision": "4de5010f750604cddd551700547f5261",
+    "url": "/Communication-platform/static/media/usos-ico.4de5010f.svg"
+  },
+  {
+    "revision": "735bc5c47ee79f1a3a4a8c7125fd7e65",
+    "url": "/Communication-platform/static/media/web-page-ico.735bc5c4.svg"
+  }
+]);
